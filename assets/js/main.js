8d3c1734f89ef8eab082f47ed2146cf13ad7503d
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-05-13 04:01:21 +0300

    E-Commerce

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,25 +242,6 @@
       clickable: true,
     },
   });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
   /**
    * Animation on scroll
    */
